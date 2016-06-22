06956e9f23beedc427ba1dc83725599147d9040d
XRayV5 <ruixu0530@gmail.com>
2016-06-22 20:59:50 +1000

    MemoGame index.html merged

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,14 @@
 console.log("JS file is connected to HTML! Woo!")
+var a, b, c, d;
+a = "King";
+b = "King";
+c = "Queen";
+d = "Queen";
+console.log(a === b);
+console.log(a === d);
+console.log(a+" "+b+" " +c+" "+d);
+
+if(a===c){
+	alert("You found a match");}
+else{
+	alert("Try again!");}
