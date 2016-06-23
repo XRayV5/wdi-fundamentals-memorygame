166c3bfe9529264a2dee6713ceff3446abcf49ae
XRayV5 <ruixu0530@gmail.com>
2016-06-23 20:57:10 +1000

    Cards added on board by js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,4 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 var a, b, c, d;
 a = "King";
 b = "King";
@@ -7,8 +7,23 @@ d = "Queen";
 console.log(a === b);
 console.log(a === d);
 console.log(a+" "+b+" " +c+" "+d);
-
+/*
 if(a===c){
 	alert("You found a match");}
 else{
-	alert("Try again!");}
+	alert("Try again!");}*/
+
+var gameBoard = document.getElementsByClassName('board')[0];
+
+gameBoard.setAttribute('id',"game-board");
+
+var addCards = function(num){
+	var card;
+	for(i=0;i<num;i++){
+		card = document.createElement('div');
+		card.setAttribute('class','card');
+		gameBoard.appendChild(card);
+		}
+	
+	}
+addCards(4);
